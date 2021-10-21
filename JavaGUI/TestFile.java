0fe870c27b3900e22880795548ff44afc8660f7c
padawan <bryce.kapp@evsck12.com>
2021-10-21 14:03:49 -0500

    trying to get the script to run when I click a button

---
+++ JavaGUI/TestFile.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.lang.InterruptedException;
+public class TestFile {
+    JButton addImageButton;
+    JButton removeImageButton;
+    public static void main(String[] args)throws IOException,InterruptedException{
+        TestFile gui = new TestFile();
+        gui.startProgram();
+    }
+    public void startProgram(){
+        JFrame frame = new JFrame();
+        addImageButton = new JButton("Add Image");//to make a button. You can pass the button constructor the text you want on the button
+        addImageButton.addActionListener(new addImage());//register your interest with the button.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this line makes the program quit as soon as you close the window (if you leave this out it will just sit there on the screen forever)
+        frame.getContentPane().add(BorderLayout.NORTH, addImageButton); //this add the button to the frame's content pane
+        frame.setSize(300,300); //gives the frame a size, in pixels
+        frame.setVisible(true); // this makes the frame visible. if you forget this step, you won't see anything when you run this code
+        
+        removeImageButton = new JButton("Remove Image");//to make a button. You can pass the button constructor the text you want on the button
+        removeImageButton.addActionListener(new removeImage());//register your interest with the button.
+        frame.getContentPane().add(BorderLayout.SOUTH, removeImageButton);
+    }
+    class addImage implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            JFileChooser chooser = new JFileChooser();
+            int status = chooser.showOpenDialog(null);
+            if (status == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                if (file == null) {
+                    return;
+                }
+                String fileName = chooser.getSelectedFile().getAbsolutePath();
+                System.out.println(fileName);
+                runScript();
+            }
+        }
+    }
+    class removeImage implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            JFileChooser chooser = new JFileChooser();
+            int status = chooser.showOpenDialog(null);
+            if (status == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                if (file == null) {
+                    return;
+                }
+                String fileName = chooser.getSelectedFile().getAbsolutePath();
+                System.out.println(fileName);
+                runScript();
+            }
+        }
+    }
+    public void runScript()throws IOException,InterruptedException{
+        Runtime runtime = Runtime.getRuntime();
+        Process proc = runtime.exec("powershell C:\\Users\\padawan\\Documents\\foyerSlideShow\\JavaGUI\\testScript.ps1");
+        InputStream is = proc.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isr);
+        String line;
+        while ((line = reader.readLine()) != null){
+            System.out.println(line);
+        }
+        reader.close();
+        proc.getOutputStream().close();
+    }
+}
---
+++ JavaGUI/testScript.ps1
@@ -0,0 +1,7 @@
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+Write-host "My directory is $dir"
+Get-ChildItem -Path $dir
+#New-Item -Path $dir'\textfile1' -ItemType file
+#Move-Item -Path 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\TestFolder1' -Destination 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\Notes'
+#Add-Content 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\why.ps1' "`nwhy not" #this is used to appand things to a file
