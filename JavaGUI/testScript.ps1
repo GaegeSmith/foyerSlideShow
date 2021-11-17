4610d56c13253d7db554dda3d2b139e50d7fc1da
padawan <bryce.kapp@evsck12.com>
2021-11-17 14:10:18 -0600

    Trying to be able to dirictly inport password to pi

--- JavaGUI/testScript.ps1
+++ JavaGUI/testScript.ps1
@@ -4,9 +4,9 @@ Write-host "My directory is $dir"
 Write-host "fileName = $fileName"
 #Get-ChildItem -Path $dir
 #New-Item -Path $dir'\textfile1' -ItemType file
-scp.exe $fileName "pi@192.168.1.100:Documents/FoyerScreens"
-
-"finish scp"
+Write-output "before scp"
+scp.exe $fileName "pi@192.168.1.100:Documents/FoyerScreens" 
+Write-output "finish scp"
 #Move-Item -Path 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\TestFolder1' -Destination 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\Notes'
 #Move-Item -Path $fileName -Destination $dir
 #Add-Content 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\why.ps1' "`nwhy not" #this is used to appand things to a file
