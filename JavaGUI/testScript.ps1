774140cd38c9973c44e8f44fd6dc1cb5ae986927
padawan <bryce.kapp@evsck12.com>
2021-11-18 14:03:14 -0600

    got the image to uploud to the pi

--- JavaGUI/testScript.ps1
+++ JavaGUI/testScript.ps1
@@ -1,11 +1,11 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-Write-host "My directory is $dir"
+#$scriptpath = $MyInvocation.MyCommand.Path
+#$dir = Split-Path $scriptpath
+#Write-host "My directory is $dir"
 Write-host "fileName = $fileName"
 #Get-ChildItem -Path $dir
 #New-Item -Path $dir'\textfile1' -ItemType file
 Write-output "before scp"
-scp.exe $fileName "pi@192.168.1.100:Documents/FoyerScreens" 
+scp.exe $fileName "pi@192.168.1.100:Documents/FoyerScreens/"
 Write-output "finish scp"
 #Move-Item -Path 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\TestFolder1' -Destination 'C:\Users\padawan\Documents\2122meterial\PLTWJava\Unit3\Notes'
 #Move-Item -Path $fileName -Destination $dir
